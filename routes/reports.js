946b9d534aaa3bcc13d407b5df03fb7a39de7b5b
Noah Tatko <tatkon@rbtrainers.com>
2020-07-17 17:47:57 -0500

    gets and posts done

--- routes/reports.js
+++ routes/reports.js
@@ -8,39 +8,38 @@ const clientPreferences = {
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-  });
-});
+router.get('/', async function(req, res, next) {
+  const client = new Client(clientPreferences);
+  await client.connect();
 
-router.post('/', async function(req, res, next) {
-
-  const { long, lat, priority, address, imageUrl } = req.body
-  const query = `INSERT INTO reports (priority, location_lat, location_lon, address, image_url, archived) VALUES ('${priority}','${lat}','${long}','${address}','${imageUrl}','false')`
+  const query = `SELECT * FROM reports`;
   try{
-    const client = new Client(clientPreferences);
-    await client.connect();
-
-    const tables = await client.query(`SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-    ORDER BY table_name;`)
-    console.log("tables", tables)
-
     const data = await client.query(query)
-    console.log("Successfully inserted", req.body)
-    console.log("data", data)
-    res.send(data)
+    res.send(data.rows)
   } catch (err) {
     console.error(err)
     next(err)
   } finally {
-    client.close()
+    client.end()
+  }
+});
+
+router.post('/', async function(req, res, next) {
+  const client = new Client(clientPreferences);
+  await client.connect();
+
+  const { long, lat, priority, address, imageUrl } = req.body
+  const query = `INSERT INTO reports (priority, location_lat, location_lon, address, image_url, archived) VALUES ('${priority}','${lat}','${long}','${address}','${imageUrl}','false');`
+  try{
+    const data = await client.query(query)
+    console.log("Successfully inserted", req.body)
+    console.log("data", data)
+    res.send('Success')
+  } catch (err) {
+    console.error(err)
+    next(err)
+  } finally {
+    client.end()
   }
 })
 
