8f28e1d346055f94c836d92fa27d9d32cb8b2197
Noah Tatko <tatkon@rbtrainers.com>
2020-07-23 12:04:21 -0500

    add a script to remove stale photos

---
+++ scripts/cleanS3.js
@@ -0,0 +1,46 @@
+// npm i node-fetch aws-sdk
+
+const fetch = require('node-fetch')
+var AWS = require('aws-sdk');
+
+AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'us-west-2'});
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+var bucketParams = {
+  Bucket : process.env.BUCKET,
+};
+
+fetch('https://geokit-api.herokuapp.com/report/')
+  .then(response => response.json())
+  .then(reports => {
+    const usedUrls = reports.map(
+      report => report.image_url.replace('https://geokit-pothole-bucket.s3.amazonaws.com/', '').replace('%3A', ":").replace('%3A', ":")
+    ).map(path => decodeURI(path))
+
+    console.log("usedURls", usedUrls)
+
+    s3.listObjects(bucketParams, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        const toDelete = data.Contents.filter(({Key}) => {
+          return !usedUrls.includes(Key)
+        }).map(obj => ({Key: obj.Key}))
+
+        console.log("toDelete", toDelete)
+        const options = {
+          ...bucketParams,
+          Delete: {
+            Objects: toDelete
+          }
+        }
+        s3.deleteObjects(options, function(err, data){
+          if(err){
+            console.log("Check with error message " + err);
+          } else {
+            console.log("Files successfully deleted", data);
+          }
+        });
+      }
+    });
+  })
